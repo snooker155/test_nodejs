f8b454d4866563385147d2d3218c0903ff43847f
snooker155 <izub.anton@gmail.com>
2016-12-02 19:36:25 +0300

    Futher implmentation

--- agxgame.js
+++ agxgame.js
@@ -1,3 +1,10 @@
 var io;
 var gameSocket;
 
+
+
+function hostCreateNewGame() {
+    var thisGameId = (Math.random() * 100000) | 0;
+    this.emit('newGameCreated', {gameId: thisGameId, mySocketId: this.id});
+    this.join(thisGameId.toString());
+}
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,11 +4,19 @@ $(function ($) {
     var IO = {
         // There are code which relate to use of Socket.IO
 
+        /**
+         * This is called when the page is displayed. It connects the Socket.IO client
+         * to the Socket.IO server
+         */
         init: function(){
             IO.socket = io.connect();
             IO.bindEvents();
-        }
+        },
 
+        /**
+         * While connected, Socket.IO will listen to the following events emitted
+         * by the Socket.IO server, then run the appropriate function.
+         */
         bindEvents: function(){
             IO.socket.on('connected', IO.onConnected );
             IO.socket.on('newGameCreated', IO.onNewGameCreated );
@@ -18,22 +26,99 @@ $(function ($) {
             IO.socket.on('hostCheckAnswer', IO.hostCheckAnswer);
             IO.socket.on('gameOver', IO.gameOver);
             IO.socket.on('error', IO.error );
+        },
+
+        /**
+         * The client is successfully connected!
+         */
+        onConnected : function() {
+            // Cache a copy of the client's socket.IO session ID on the App
+            App.mySocketId = IO.socket.socket.sessionid;
+            // console.log(data.message);
+        },
+
+        /**
+         * A new game has been created and a random game ID has been generated.
+         * @param data {{ gameId: int, mySocketId: * }}
+         */
+        onNewGameCreated : function(data) {
+            App.Host.gameInit(data);
+        },
+
+        /**
+         * A player has successfully joined the game.
+         * @param data {{playerName: string, gameId: int, mySocketId: int}}
+         */
+        playerJoinedRoom : function(data) {
+            // When a player joins a room, do the updateWaitingScreen funciton.
+            // There are two versions of this function: one for the 'host' and
+            // another for the 'player'.
+            //
+            // So on the 'host' browser window, the App.Host.updateWiatingScreen function is called.
+            // And on the player's browser, App.Player.updateWaitingScreen is called.
+            App[App.myRole].updateWaitingScreen(data);
+        },
+
+        /**
+         * Both players have joined the game.
+         * @param data
+         */
+        beginNewGame : function(data) {
+            App[App.myRole].gameCountdown(data);
+        },
+
+        /**
+         * A new set of words for the round is returned from the server.
+         * @param data
+         */
+        onNewWordData : function(data) {
+            // Update the current round
+            App.currentRound = data.round;
+
+            // Change the word for the Host and Player
+            App[App.myRole].newWord(data);
+        },
+
+        /**
+         * A player answered. If this is the host, check the answer.
+         * @param data
+         */
+        hostCheckAnswer : function(data) {
+            if(App.myRole === 'Host') {
+                App.Host.checkAnswer(data);
+            }
+        },
+
+        /**
+         * Let everyone know the game has ended.
+         * @param data
+         */
+        gameOver : function(data) {
+            App[App.myRole].endGame(data);
+        },
+
+        /**
+         * An error has occurred.
+         * @param data
+         */
+        error : function(data) {
+            alert(data.message);
         }
-    }
+    };
 
     var App = {
         // There is a basic app logic
 
         init: function(){
 
-        }
+        },
 
-        Host {
+        Host: {
             // Game logic for main screen
-        }
+        },
 
-        Player {
+        Player: {
             // Game logic for player screen
-        }
-    }
+        },
+    };
 });
